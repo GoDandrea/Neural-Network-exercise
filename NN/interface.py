ea8db85ab102e7157d72ff2dbdcefa31d252dc6f
GoDandrea <dandrea.alves@gmail.com>
2020-08-17 16:25:05 -0300

    Melhora de interface

--- NN/interface.py
+++ NN/interface.py
@@ -6,27 +6,31 @@ consultas na rede treinada.
 
 def predict_results(network, device, dim, dhl, tgo, tgp):
     entry = torch.as_tensor([dim, dhl, tgo, tgp]).float().to(device)
-    output = network(entry)
-    return output.ge(0.5).item()
+    output = network(entry).ge(0.5)
+    output = output.tolist()
+    return output
 
 def use_network(path, device):
     network = torch.load(path)
-    message1 = '>>> Entre com os resultados dos exames necessários (apenas pontos e dígitos)\n'
+    message1 = '\n>>> Entre com os resultados dos exames necessários (apenas pontos e dígitos)\n'
     message2 = '>>> [Formato: D-Dímero/Desidrogenase Láctica (DHL)/TGO/TGP]\n>>> '
     answer = input(message1+message2).split('/')
-    if answer[0] == 'e':
+    if (answer[0] == 'E') or (answer[0] == 'e'):
         return False
     dim = float(answer[0])
     dhl = float(answer[1])
     tgo = float(answer[2])
     tgp = float(answer[3])
-    prediction == predict_results(network, device, dim, dhl, tgo, tgp)
-    print('Previsão de exames de PCR, IgG, e IgM:\n', prediction)
+    prediction = predict_results(network, device, dim, dhl, tgo, tgp)
+    print('\n>>> Previsão de exames de PCR, IgG, e IgM:')
+    print('>>> PCR: ' + ('Positivo' if prediction[0] else 'Negativo'))
+    print('>>> IgG: ' + ('Positivo' if prediction[1] else 'Negativo'))
+    print('>>> IgM: ' + ('Positivo' if prediction[2] else 'Negativo'))
     return True
 
 def use_iteratively(path, device):
-    print('>>> Modelo carregado de ' + path)
-    print('>>> (use "e" para sair)\n')
+    print('\n>>> Modelo carregado de ' + path)
+    print('>>> (use "E" para sair)')
     keep_going = True
     while keep_going:
         keep_going = use_network(path, device)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PRE.pre_process import get_einstein, get_fleury, join_data
 from NN.neural_net import FFN_2HLayers, split_data, train_network
-from interface import use_iteratively
+from NN.interface import use_iteratively
 import torch
 
 import pandas as pd
@@ -19,39 +19,39 @@ MODEL_PATH = 'FFN_2HL_COVID.pt'
 if __name__ == '__main__':
 
     message1 = '>>> Entre "U" para utilizar a rede em '
-    message2 = '>>> Entre "T" para treinar uma rede nova:\n>>>'
+    message2 = '\n>>> Entre "T" para treinar uma rede nova:\n>>> '
     mode = input(message1+MODEL_PATH+message2)
 
-    if (mode != 'U') and (mode != 'T'):
-        print('Modo', mode, 'inválido. Encerrando programa.\n')
-    elif (mode == 'U'):
+    if mode not in ['U', 'u', 'T', 't']:
+        print('>>> Modo', mode, 'inválido. Encerrando programa.\n')
+    elif (mode == 'U') or (mode == 'u'):
         device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         use_iteratively(MODEL_PATH, device)
-        print('Encerrando programa.\n')
+        print('>>> Encerrando programa.\n')
 
     else:
         try:
-            print("Carregando dados prá-processados")
+            print(">>> Carregando dados prá-processados")
             processed_data = pd.read_csv('PRE/treated.csv', sep='|')
         except:
-            print("Dados pré-processados não encontrados")
-            print("Carregando dados do Einstein")
+            print(">>> Dados pré-processados não encontrados")
+            print(">>> Carregando dados do Einstein")
             ein_data = get_einstein('PRE/dados/einstein_exames.csv')
-            print("Carregando dados do Fleury")
+            print(">>> Carregando dados do Fleury")
             fleury_data = get_fleury('PRE/dados/fleury_exames.csv')
-            print("Unificando dados")
+            print(">>> Unificando dados")
             processed_data = join_data(ein_data, fleury_data)
             
-        print("Separando dados para treino")
+        print(">>> Separando dados para treino")
         splitted_data = split_data(processed_data)
 
-        print("Montando modelo")
+        print(">>> Montando modelo")
         x_train = splitted_data[0]
         network = FFN_2HLayers(x_train.shape[1], N_HL1, N_HL2)
         optimizer = torch.optim.Adam(network.parameters(), lr=LEARNING_RATE)
         criteria = torch.nn.BCELoss()
 
-        print('Treinando...')
+        print('>>> Treinando...')
         train_network(splitted_data, network, optimizer, criteria, MODEL_PATH)
-        print('Modelo treinado com sucesso.\nEstá salvo em ' + MODEL_PATH)
+        print('>>> Modelo treinado com sucesso.\n>>> Está salvo em ' + MODEL_PATH)
 
